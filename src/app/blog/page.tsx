fcaa41da0e096835edfd4d100f74b444f77208e0
itsfingerlickinggood <sonuipad05@gmail.com>
2025-01-03 23:07:20 +0530

    0.4

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Blog = () => {
+  return (
+    <div>
+      <h2>File</h2>
+      <p>This is the File component.</p>
+    </div>
+  );
+};
+
+export default Blog;
