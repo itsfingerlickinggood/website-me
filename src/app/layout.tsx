207b497aa11f800c46f4712e89835148c5aaedec
itsfingerlickinggood <sonuipad05@gmail.com>
2025-01-03 23:47:02 +0530

    0.5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logo-w.jpg" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
